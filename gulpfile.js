8b2c9d3671de5d942006399dfb2a9348da8d7e6d
Matthew Grill <matt@fourkitchens.com>
2014-12-21 14:00:13 -0800

    Restructure Gulp file
    Some cleanup in Gulpfile format.

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var gulp = require('gulp'),
     jscs = require('gulp-jscs'),
     merge = require('merge-stream'),
     gutil = require('gulp-util'),
-    clean = require('gulp-clean'),
+    del = require('del'),
     options = require('minimist')(process.argv.slice(2));
 
 /**
@@ -66,7 +66,7 @@ gulp.task('phpcs', function () {
  * @task build
  * Constructs a Drupal site, executes construction commands, and builds a
  * functional Drupal site root.
- * 
+ *
  * @param string options.builddir
  *   Name of buidls subdirectory in which this project should be built.
  */
@@ -77,22 +77,15 @@ gulp.task('build', function() {
 
   var builddir = 'builds/' + options.builddir;
 
-  // Remove existing builddirs.
-  var cleanBuildDir = gulp.src('builds/' + options.builddir)
-  .pipe(clean());
+  del([builddir]);
 
-  // Make the new (empty) directory.
-  var makeBuildDir = gulp.src('drupal.make')
-  .pipe(shell('mkdir ' + builddir))
-
-  // Run drush make in the directory.
-  var drushMake = gulp.src('drupal.make')
-  .pipe(shell('cd ' + builddir + ' && drush make ../../drupal.make -y'));
-
-  var settings = gulp.src('local.settings.php')
-  .pipe(gulp.dest(builddir + '/sites/default'));
-
-  return merge(cleanBuildDir, makeBuildDir, drushMake, settings);
+  return merge(
+      gulp.src('drupal.make')
+        .pipe(shell('mkdir -p ' + builddir))
+        .pipe(shell('cd ' + builddir + ' && drush make ../../drupal.make -y')),
+      gulp.src(['site.settings.php','local.settings.php'])
+        .pipe(gulp.dest(builddir + '/sites/default'))
+    );
 });
 
 /**
@@ -101,15 +94,13 @@ gulp.task('build', function() {
  * functional Drupal site root.
  */
 gulp.task('setup', function() {
-  var cleanworkdir = gulp.src([
+  del([
     'builds/workdir/**/*',
     '!builds/workdir/README.md'
-  ])
-  .pipe(clean());
+  ]);
 
-  var build = gulp.src('drupal.make').pipe(shell('gulp build --builddir workdir'));
-
-  return merge(cleanworkdir, build);
+  return gulp.src('drupal.make')
+          .pipe(shell('gulp build --builddir workdir'));
 });
 
 /**
@@ -118,3 +109,4 @@ gulp.task('setup', function() {
  * @task deploy
  *   Builds, packages, and then deployes.
  */
+
